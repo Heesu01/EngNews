9189739a6f9b48ff8bcc4294006e8f9cc3147978
Heesu01 <rulru0413@naver.com>
2024-11-06 03:17:49 +0900

    setting: 글로벌스타일, 테마 생성

---
+++ src/styles/Theme.js
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+export const Theme = {
+  fonts: {
+    default: css``,
+    logo: css``,
+    title: css``,
+    others: css``,
+  },
+  colors: {
+    white: "#fff",
+    black: "#000",
+    gray: "#9A9A9A",
+    gray2: "#E9E9E9",
+  },
+  breakpoints: {},
+};
