1ebb8cbbeb86d646c25fe27a89255ed4d265f535
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-11-09 02:51:04 +0900

    Revert "Feature/login"

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -8,7 +8,6 @@ export const Theme = {
     logo: css`
       font-family: "RiaSans-ExtraBold";
       font-size: 25px;
-      color: #091057;
     `,
     title: css``,
     others: css``,
@@ -20,11 +19,6 @@ export const Theme = {
     gray: "#9A9A9A",
     gray2: "#c4c4c4",
     gray3: "#E9E9E9",
-    logoNavy: "#091057",
-    navy: "#102C57",
-    blue: "#133E87",
-    deepBlue: "#4A628A",
-    lightBlue: "#CBDCEB",
   },
   breakpoints: {},
 };
