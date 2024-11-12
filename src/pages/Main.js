48e4ccd4ccc1fcdde46465c721cf761b2ed5f3f9
Heesu01 <rulru0413@naver.com>
2024-11-13 04:08:44 +0900

    feat: 메인페이지 UI 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,23 @@
 import styled from "styled-components";
-
 import Banner from "../components/Banner";
 
 const Main = () => {
+  const naverArticles = [
+    { id: 1, title: "네이버 기사 1" },
+    { id: 2, title: "네이버 기사 2" },
+    { id: 3, title: "네이버 기사 3" },
+    { id: 4, title: "네이버 기사 4" },
+    { id: 5, title: "네이버 기사 5" },
+  ];
+
+  const nyTimesArticles = [
+    { id: 1, title: "NY Times 기사 1" },
+    { id: 2, title: "NY Times 기사 2" },
+    { id: 3, title: "NY Times 기사 3" },
+    { id: 4, title: "NY Times 기사 4" },
+    { id: 5, title: "NY Times 기사 5" },
+  ];
+
   return (
     <Container>
       <Banner />
@@ -12,13 +27,27 @@ const Main = () => {
             <Title>
               네이버 TOP 5 <span>자세히보기 ></span>
             </Title>
-            <Card></Card>
+            <CardWrapper>
+              {naverArticles.map((article, index) => (
+                <Article key={article.id}>
+                  <Rank>{index + 1}</Rank>
+                  <ArticleTitle>{article.title}</ArticleTitle>
+                </Article>
+              ))}
+            </CardWrapper>
           </CardBox>
           <CardBox>
             <Title>
               NY Times TOP 5 <span>자세히보기 ></span>
             </Title>
-            <Card></Card>
+            <CardWrapper>
+              {nyTimesArticles.map((article, index) => (
+                <Article key={article.id}>
+                  <Rank>{index + 1}</Rank>
+                  <ArticleTitle>{article.title}</ArticleTitle>
+                </Article>
+              ))}
+            </CardWrapper>
           </CardBox>
         </CardContainer>
       </BottomContainer>
@@ -39,10 +68,10 @@ const CardContainer = styled.div`
   justify-content: center;
 `;
 const CardBox = styled.div`
-  width: 50%;
+  width: 45%;
 `;
 const Title = styled.div`
-  font-size: 20px;
+  font-size: 22px;
   font-weight: 600;
   margin-bottom: 10px;
   padding: 0 5px;
@@ -57,10 +86,32 @@ const Title = styled.div`
     margin-top: 10px;
   }
 `;
-const Card = styled.div`
+const CardWrapper = styled.div`
   width: 100%;
   height: 500px;
   border: 1px solid ${(props) => props.theme.colors.navy};
   border-radius: 5px;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
 `;
+
+const Article = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+`;
+
+const Rank = styled.div`
+  font-size: 22px;
+  font-weight: bold;
+  margin-right: 15px;
+  color: ${(props) => props.theme.colors.blue};
+`;
+
+const ArticleTitle = styled.div`
+  font-size: 20px;
+`;
+
 export default Main;
