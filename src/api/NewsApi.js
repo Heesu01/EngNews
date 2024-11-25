0deaa8a06e82b93d70798cf2b8847f95c07d6d30
Heesu01 <rulru0413@naver.com>
2024-11-25 23:05:02 +0900

    feat: 카테고리별, 키워드별 기사 조회 api 파일 추가

--- src/api/NewsApi.js
+++ src/api/NewsApi.js
@@ -17,3 +17,42 @@ export const fetchNytTop5News = async () => {
     throw error.response?.data || error.message;
   }
 };
+
+export const fetchNaverArticlesByCategory = async (category, page) => {
+  try {
+    const response = await Axios.get(`/news/naver/categories`, {
+      params: { category, page },
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error.message;
+  }
+};
+
+export const fetchNytArticlesByCategory = async (category, page) => {
+  try {
+    const response = await Axios.get(`/news/nyt/categories`, {
+      params: { category, page },
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error.message;
+  }
+};
+
+export const fetchNaverArticlesByKeyword = async () => {
+  try {
+    const response = await Axios.get("/news/naver/keyword");
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error.message;
+  }
+};
+export const fetchNytArticlesByKeyword = async () => {
+  try {
+    const response = await Axios.get("/news/nyt/keyword");
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error.message;
+  }
+};
